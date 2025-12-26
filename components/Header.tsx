0e9c66fd0d49031c5ef2e3489ee8cf773fc4d5a9
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-12-26 21:49:24 +0530

    Add herder

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,40 @@
+'use client';
+
 import { Button } from './ui/button';
 import { Sun } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className="flex w-full bg-red-400 p-2">
-      <Button variant="secondary" size="icon" className="rounded-full">
-        <Sun />
-      </Button>
+    <header className="fixed flex w-full justify-between p-3 backdrop-blur-md">
+      <div
+        className="flex cursor-pointer items-center gap-3"
+        onClick={() => router.push('/')}
+      >
+        <Image
+          src="/logo-solid.jpeg"
+          alt="Astra agent logo"
+          width={50}
+          height={50}
+          className="rounded"
+        />
+        <p className="text-2xl font-semibold tracking-wide">Astra Agent</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <Button
+          variant="secondary"
+          size="icon"
+          className="rounded-full text-xl"
+        >
+          <Sun size={24} strokeWidth={2} />
+        </Button>
+        <Button variant="secondary" className="text-md">
+          Get Started
+        </Button>
+      </div>
     </header>
   );
 };
