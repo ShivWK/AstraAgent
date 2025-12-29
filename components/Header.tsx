1e9268dbad1c1394bfaa3491a8dba590a0c0b5c3
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-12-29 21:55:03 +0530

    reset the form when switched bwtween sign and signup

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useTheme } from 'next-themes';
 import ThemeChanger from './ThemeChanger';
-import { LoginForm } from './LoginForm';
+import { LoginForm } from './auth/LoginForm';
 
 const Header = () => {
   const [openLoginForm, setOpenLoginForm] = useState(false);
