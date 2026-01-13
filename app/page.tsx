bc756ed636fafdb7ffcef2ba77f58515ae4fe622
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-01-13 20:07:25 +0530

    feat:Change home pager style a bit and remove an agent card

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
-      <section className="flex flex-col items-center gap-2 p-2">
+      <section className="flex flex-col items-center gap-3 p-2">
         <Image
           src="/logo-transparent.png"
           alt="Astra agent site logo, an start in blue shade"
           width={250}
           height={250}
-          className="rounded border"
+          className="rounded"
         ></Image>
         <span className="text-center text-xl">Your AI task Force</span>
         <Button
@@ -29,10 +29,6 @@ export default function Home() {
         </Button>
         <span className="text-center">Unlock powerful agents</span>
         <div className="flex flex-wrap gap-3">
-          <div className="flex flex-col items-center gap-1 rounded border border-blue-400 p-2">
-            <Cat />
-            <span>agent</span>
-          </div>
           <div className="flex flex-col items-center gap-1 rounded border border-blue-400 p-2">
             <Cat />
             <span>agent</span>
