ba192b8d325f9f7eddf808e82ea205ef4cbccfeb
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-01-31 20:28:16 +0530

    Add Previous chats and feedback section

--- app/ai-assistant/page.tsx
+++ app/ai-assistant/page.tsx
@@ -33,38 +33,54 @@ const AiAssistant = () => {
   }, [dispatch, mode1, mode2, router]);
 
   return (
-    <main>
-      <section className="mx-auto max-w-[1200px] pt-22 pb-18 md:pt-25">
-        <div className="flex flex-col items-center justify-between gap-2 md:flex-row">
-          <div>
-            <h1 className="text-3xl font-bold md:text-2xl dark:text-white">
-              Welcome to the world of AI Assistant
-            </h1>
-            <p className="text-xl">
-              Choose your AI companion to simplify your task
-            </p>
+    <main className="pb-18">
+      <div className="mx-auto max-w-[1200px] pt-22 md:pt-25">
+        <section className="w-full">
+          <div className="flex flex-col items-center justify-between gap-2 md:flex-row">
+            <div>
+              <h1 className="text-3xl font-bold md:text-2xl dark:text-white">
+                Welcome to the world of AI Assistant
+              </h1>
+              <p className="text-xl">
+                Choose your AI companion to simplify your task
+              </p>
+            </div>
+            <Button
+              variant={'secondary'}
+              disabled={selectedModel.length === 0}
+              className={`${selectedModel.length !== 0 && styles['btn-continue']} rounded-full py-5 text-lg font-normal hover:-translate-y-0.5 active:translate-y-0 max-md:hidden`}
+            >
+              Start Session
+            </Button>
           </div>
+          <ModelCards
+            mode={mode2}
+            assistants={mode2 === 'speech' ? voice_assistant : text_assistant}
+          />
           <Button
             variant={'secondary'}
             disabled={selectedModel.length === 0}
-            className={`${selectedModel.length !== 0 && styles['btn-continue']} rounded-full py-5 text-lg font-normal hover:-translate-y-0.5 active:translate-y-0 max-md:hidden`}
+            className={`${selectedModel.length !== 0 && styles['btn-continue']} mx-auto mt-8 flex rounded-full py-6 text-lg font-normal active:scale-95 md:hidden`}
           >
             Start Session
           </Button>
-        </div>
-        <ModelCards
-          mode={mode2}
-          assistants={mode2 === 'speech' ? voice_assistant : text_assistant}
-        />
-        <Button
-          variant={'secondary'}
-          disabled={selectedModel.length === 0}
-          className={`${selectedModel.length !== 0 && styles['btn-continue']} mx-auto mt-8 flex rounded-full py-6 text-lg font-normal active:scale-95 md:hidden`}
-        >
-          Start Session
-        </Button>
-      </section>
-      <section className="section__history"></section>
+        </section>
+        <section className="section__history mt-10 flex flex-col gap-8 md:mx-auto md:mt-15 md:max-w-4xl md:flex-row md:justify-between md:gap-50">
+          <div className="rounded-xl bg-blue-900 px-3 py-2 max-md:text-center md:basis-1/2">
+            <h2 className="mb-2 text-xl font-semibold md:text-2xl dark:text-white">
+              Previous Conversations
+            </h2>
+            <p>No previous conversations</p>
+          </div>
+
+          <div className="rounded-xl bg-blue-900 px-3 py-2 max-md:text-center md:basis-1/2">
+            <h2 className="mb-2 text-xl font-semibold md:text-2xl dark:text-white">
+              Feedback of Conversations
+            </h2>
+            <p>No feedback</p>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
