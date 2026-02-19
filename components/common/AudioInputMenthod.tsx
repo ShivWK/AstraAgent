bbac392516c08fb7b7bcfb80a47483b94b1d1845
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-02-19 18:33:48 +0530

    integrate the stop listening button in the start listening button

--- components/common/AudioInputMenthod.tsx
+++ components/common/AudioInputMenthod.tsx
@@ -2,7 +2,7 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from 'react-speech-recognition';
 import { useEffect, useState } from 'react';
-import { Mic } from 'lucide-react';
+import { Mic, CircleStop } from 'lucide-react';
 
 const AudioInputMethod = () => {
   const [level, setLevel] = useState(0);
@@ -70,12 +70,12 @@ const AudioInputMethod = () => {
     return <span>Browser doe not support speech recognition.</span>;
   }
 
-  const startListening = () => {
-    SpeechRecognition.startListening({ continuous: true });
-  };
-
-  const stopListening = () => {
-    SpeechRecognition.stopListening();
+  const micClickHandler = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening({ continuous: true });
+    }
   };
 
   console.log('level', level);
@@ -84,23 +84,19 @@ const AudioInputMethod = () => {
     <div className="rounded-primary relative flex w-full items-center justify-center bg-linear-to-b from-black via-blue-600/30 to-black p-9">
       <button
         aria-label="microphone"
-        onClick={startListening}
+        onClick={micClickHandler}
         style={{
           boxShadow: `0 0 ${level / 2}px rgba(59,130,246,0.8)`,
           transform: listening ? `scale(${1 + level / 200})` : '',
         }}
         className={`${listening && 'btn-continue'} btn-continue no-hover transform rounded-full bg-blue-900 p-3 transition-transform duration-75 ease-in-out before:h-[108%] before:w-[108%] after:h-[108%] after:w-[108%] active:scale-95 md:p-4 md:before:h-[110%] md:before:w-[110%] md:after:h-[110%] md:after:w-[110%]`}
       >
-        <Mic aria-hidden="true" size={70} strokeWidth={1} />
+        {listening ? (
+          <CircleStop aria-hidden="true" size={70} strokeWidth={1} />
+        ) : (
+          <Mic aria-hidden="true" size={70} strokeWidth={1} />
+        )}
       </button>
-      {listening && (
-        <button
-          onClick={stopListening}
-          className="rounded-primary absolute top-34 bg-red-700 px-2 py-0.5 md:top-36"
-        >
-          Stop
-        </button>
-      )}
     </div>
   );
 };
