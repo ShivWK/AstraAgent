4b1d8367fde0001e0f23a366b279f5902c792a69
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-02-24 14:55:20 +0530

    change some style

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -61,7 +61,7 @@ const Header = ({ isUserLoggedIn }: PropsType) => {
 
   return (
     <>
-      <header className="fixed z-50 flex w-full justify-between px-2.5 py-3 backdrop-blur-md">
+      <header className="fixed z-50 flex w-full justify-between px-2.5 py-1.5 backdrop-blur-md">
         <button
           onClick={() => dispatch(setOpenSidebar(true))}
           className={`${pathname === '/ai-workspace' ? 'block md:hidden' : 'hidden'} rounded-full bg-blue-900 p-3`}
