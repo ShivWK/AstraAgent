f81b7648b3276cf497b2977889c550ceb3618cd8
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-02-28 13:43:46 +0530

    Fix: profile picture change form

--- components/auth/ProfileChange.tsx
+++ components/auth/ProfileChange.tsx
@@ -5,9 +5,13 @@ import { Pencil, Save } from 'lucide-react';
 import * as z from 'zod';
 import { profileFormSchema } from '@/lib/validations/agents.schema';
 import { Spinner } from '../ui/spinner';
+import { useEffect, useState } from 'react';
 
 const ProfileChange = () => {
   type FormType = z.infer<typeof profileFormSchema>;
+  const [previewURL, setPreviewURl] = useState<string>(
+    '/assistants/general_ai.png',
+  );
 
   const {
     register,
@@ -23,10 +27,15 @@ const ProfileChange = () => {
     // }
   });
 
-  const profileValue = watch('profileImage');
+  useEffect(() => {
+    return () => {
+      if (previewURL) {
+        URL.revokeObjectURL(previewURL);
+      }
+    };
+  }, [previewURL]);
 
   const submitHandler = (data: FormType) => {
-    // on successful submission clear the profileValue value
     console.log(data);
   };
 
@@ -34,42 +43,22 @@ const ProfileChange = () => {
     <form onSubmit={handleSubmit(submitHandler)}>
       <div className="relative">
         <Image
-          src={
-            profileValue?.name
-              ? `/assistants/${profileValue?.name}`
-              : '/assistants/general_ai.png'
-          }
+          src={previewURL}
           alt="Profile picture"
           height={300}
           width={300}
           quality={100}
           className="h-32 w-32 rounded-full shadow-2xl"
         />
-        {profileValue?.name ? (
-          <button
-            type="submit"
-            aria-label="Save Profile Picture"
-            title="Save"
-            className="absolute top-24 left-22 rounded-full bg-gray-900 p-2 transition-all duration-150 ease-linear active:scale-95"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? (
-              <Spinner className="size-5" />
-            ) : (
-              <Save aria-hidden="true" strokeWidth={1.5} size={20} />
-            )}
-          </button>
-        ) : (
-          <label
-            htmlFor="profile"
-            aria-label="Upload profile picture"
-            className="cursor-pointer"
-          >
-            <div className="absolute top-24 left-22 inline-block rounded-full bg-gray-900 p-2">
-              <Pencil aria-hidden="true" strokeWidth={1.5} size={20} />
-            </div>
-          </label>
-        )}
+        <label
+          htmlFor="profile"
+          aria-label="Upload profile picture"
+          className="cursor-pointer"
+        >
+          <div className="absolute top-24 left-22 inline-block rounded-full bg-gray-900 p-2 transition-all duration-150 ease-linear active:scale-90">
+            <Pencil aria-hidden="true" strokeWidth={1.5} size={20} />
+          </div>
+        </label>
         <input
           {...register('profileImage')}
           type="file"
@@ -78,10 +67,36 @@ const ProfileChange = () => {
           onChange={(e) => {
             const file = e.target.files?.[0];
             if (file) {
-              setValue('profileImage', file);
+              setValue('profileImage', file, {
+                shouldValidate: true,
+              });
+
+              const objectURl = URL.createObjectURL(file);
+              setPreviewURl(objectURl);
             }
           }}
         />
+
+        <div
+          className={`w-full transition-all duration-150 ease-linear ${previewURL !== '/assistants/general_ai.png' ? 'h-13' : 'h-0'} overflow-hidden`}
+        >
+          <button
+            type="submit"
+            className="mx-auto mt-4 flex items-center gap-2 rounded-md bg-gray-900 px-3 py-1 transition-all duration-150 ease-linear active:scale-90"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? (
+              <Spinner
+                aria-hidden="true"
+                data-icon="inline-start"
+                className="size-6"
+              />
+            ) : (
+              <Save aria-hidden="true" strokeWidth={1.5} size={20} />
+            )}
+            Save
+          </button>
+        </div>
       </div>
     </form>
   );
