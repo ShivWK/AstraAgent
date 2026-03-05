4f8d59c5abd56ae8c738f64791c2497230c6139a
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-03-05 19:54:48 +0530

    fix: code bug

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,9 +1,7 @@
 import * as z from 'zod';
 import { NextResponse } from 'next/server';
-import { verifySession } from '@/lib/verifySession';
 import { fileSchema } from '@/lib/validations/agents.schema';
 import { UserModel } from '@/model/userModel';
-import { SessionModel } from '@/model/sessionModel';
 import { connectDB } from '@/lib/db/connectDb';
 import { auth } from '@/auth';
 
