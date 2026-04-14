7a0214712d40e475fe4d223b28a711a2737378f5
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-04-14 16:51:40 +0530

    Update style and remove the addede div from  the skeleton

--- components/skeletons/PreviousChatSkeleton.tsx
+++ components/skeletons/PreviousChatSkeleton.tsx
@@ -1,13 +1,12 @@
 const PreviousChatSkeleton = () => {
   return (
     <div className="w-full">
-      <div className="mt-0.5 mb-3 h-7 w-[46%] animate-pulse rounded bg-gray-300 dark:bg-white/20" />
+      <div className="mt-1 mb-1.5 h-7 w-[46%] animate-pulse rounded bg-gray-300 dark:bg-white/20" />
 
-      <div className="flex w-full flex-col gap-2">
-        <div className="h-6 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
-        <div className="h-6 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
-        <div className="h-6 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
-        <div className="h-6 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
+      <div className="flex w-full flex-col gap-1">
+        <div className="h-7 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
+        <div className="h-7 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
+        <div className="h-7 w-full animate-pulse rounded bg-gray-300 dark:bg-white/20" />
       </div>
     </div>
   );
