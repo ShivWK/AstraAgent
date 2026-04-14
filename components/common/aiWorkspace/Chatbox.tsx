2345f9b110e012c9507a1558951803632a1df873
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-04-15 03:08:51 +0530

    fix: chatbox bottom extra padding

--- components/common/aiWorkspace/Chatbox.tsx
+++ components/common/aiWorkspace/Chatbox.tsx
@@ -50,7 +50,7 @@ const ChatBox = ({ writer, chat }: PropsType) => {
       className={`mb-5 flex w-full ${isUser ? 'justify-end' : 'w-full justify-start'}`}
     >
       <div className="relative max-w-[85%] rounded-xl bg-gray-600/50 p-1 pb-10">
-        <div className="pretty-scrollbar w-full overflow-x-auto rounded-xl bg-blue-900 p-3 pb-10 leading-6 wrap-break-word text-white">
+        <div className="pretty-scrollbar w-full overflow-x-auto rounded-xl bg-blue-900 p-3 pb-3 leading-6 wrap-break-word text-white">
           {writer === 'user' ? (
             <p className="wrap-break-word whitespace-pre-wrap">{chat}</p>
           ) : (
