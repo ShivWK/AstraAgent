b51bd5812bf5149a1645dae6ce481869706d24b1
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-05-11 16:48:06 +0530

    Add colors to the theme button

--- components/theme/ThemeToggleBtn.tsx
+++ components/theme/ThemeToggleBtn.tsx
@@ -45,17 +45,17 @@ const ThemeToggleBtn = () => {
     <div className="group relative">
       <button
         onClick={() => setShowDropDown(!showDropDown)}
-        className="flex cursor-pointer items-center justify-center rounded-md border-2 p-1.5 dark:border-blue-400"
+        className="border-theme-btn-border bg-theme-btn-background hover:bg-theme-btn-hover-bg shadow-theme-btn-shadow flex transform items-center justify-center rounded-md border-2 p-1.5 transition-all duration-100 ease-linear hover:-translate-y-0.5 active:translate-y-0"
       >
         {resolvedTheme === 'light' ? (
           <Sun
             strokeWidth={1.5}
-            className="size-5 transform transition-transform duration-150 ease-linear group-hover:text-[#ff5200] active:scale-95"
+            className="text-theme-btn-icon size-5 transform transition-transform duration-150 ease-linear"
           />
         ) : (
           <Moon
             strokeWidth={1.5}
-            className="size-5 transform transition-transform duration-150 ease-linear group-hover:text-[#ff5200] active:scale-95"
+            className="text-theme-btn-icon size-5 transform transition-transform duration-150 ease-linear"
           />
         )}
       </button>
@@ -63,15 +63,15 @@ const ThemeToggleBtn = () => {
       {showDropDown && (
         <div
           ref={popupRef}
-          className="absolute top-11 left-1/2 -translate-x-1/2 transform rounded bg-white p-1 drop-shadow-[0_0_5px_#6a7282] dark:bg-gray-800"
+          className="bg-theme-btn-dropdown absolute top-11 left-1/2 -translate-x-1/2 transform rounded p-1 drop-shadow-[0_0_5px_#6a7282] backdrop-blur-md"
         >
           <div className="relative text-sm">
             <ul className="list-none">
               <li
                 onClick={() => clickHandler('light')}
-                className="hover:bg-primary flex cursor-pointer items-center gap-2 rounded px-3.5 py-1.5 transition-all duration-100 hover:text-white md:px-3 md:py-1"
+                className="hover:bg-primary flex cursor-pointer items-center gap-2 rounded px-3.5 py-1.5 transition-all duration-100 hover:text-white md:px-3 md:py-1 hover:dark:text-black"
                 style={{
-                  backgroundColor: theme === 'light' ? '#e5e7eb' : '',
+                  backgroundColor: theme === 'light' ? '#cdcfd3' : '',
                   color: theme === 'light' ? 'black' : '',
                 }}
               >
@@ -80,9 +80,9 @@ const ThemeToggleBtn = () => {
               </li>
               <li
                 onClick={() => clickHandler('dark')}
-                className="hover:bg-primary mt-0.5 flex cursor-pointer items-center gap-2 rounded px-3.5 py-1.5 transition-all duration-100 hover:text-white md:px-3 md:py-1"
+                className="hover:bg-primary mt-0.5 flex cursor-pointer items-center gap-2 rounded px-3.5 py-1.5 transition-all duration-100 hover:text-white md:px-3 md:py-1 hover:dark:text-black"
                 style={{
-                  backgroundColor: theme === 'dark' ? '#e5e7eb' : '',
+                  backgroundColor: theme === 'dark' ? '#cdcfd3' : '',
                   color: theme === 'dark' ? 'black' : '',
                 }}
               >
@@ -91,9 +91,9 @@ const ThemeToggleBtn = () => {
               </li>
               <li
                 onClick={() => clickHandler('system')}
-                className="hover:bg-primary mt-0.5 flex cursor-pointer items-center gap-2 rounded px-3.5 py-1.5 transition-all duration-100 hover:text-white md:px-3 md:py-1"
+                className="hover:bg-primary mt-0.5 flex cursor-pointer items-center gap-2 rounded px-3.5 py-1.5 transition-all duration-100 hover:text-white md:px-3 md:py-1 hover:dark:text-black"
                 style={{
-                  backgroundColor: theme === 'system' ? '#e5e7eb' : '',
+                  backgroundColor: theme === 'system' ? '#cdcfd3' : '',
                   color: theme === 'system' ? 'black' : '',
                 }}
               >
@@ -114,8 +114,8 @@ const ThemeToggleBtn = () => {
             </ul>
             <div
               id="triangle"
-              className="absolute -top-3 left-1/2 h-0 w-0 -translate-x-1/2 border-r-12 border-b-12 border-l-12 border-r-transparent border-b-white border-l-transparent dark:border-b-gray-800"
-            ></div>
+              className="border-b-theme-btn-dropdown absolute -top-3 left-1/2 -z-1 h-0 w-0 -translate-x-1/2 border-r-12 border-b-12 border-l-12 border-r-transparent border-l-transparent"
+            />
           </div>
         </div>
       )}
