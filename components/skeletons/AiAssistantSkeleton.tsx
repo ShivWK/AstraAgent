3702d35f2eb7d8fc660b424791e362484e4186cf
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-05-12 22:40:47 +0530

    Refactor code

--- components/skeletons/AiAssistantSkeleton.tsx
+++ components/skeletons/AiAssistantSkeleton.tsx
@@ -2,7 +2,7 @@ import AgentCardsSkeleton from './AgentCardsSkeleton';
 
 const AiAssistantSkeleton = () => {
   return (
-    <main className="h-dvh animate-pulse pb-18">
+    <main className="min-h-dvh animate-pulse pb-18">
       <div className="mx-auto max-w-300">
         <section className="w-full">
           <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
@@ -19,16 +19,16 @@ const AiAssistantSkeleton = () => {
           <div className="mx-auto mt-8 h-12 w-40 rounded-full bg-gray-300 md:hidden dark:bg-gray-700" />
         </section>
 
-        <section className="mx-auto mt-10 flex w-full flex-col gap-8 md:w-[80%] md:gap-4">
-          <div className="w-[45%] rounded-xl bg-gray-300 p-4 md:basis-1/2 dark:bg-gray-700">
+        <section className="mx-auto mt-10 flex w-full flex-col gap-4 md:w-[80%]">
+          <div className="w-full rounded-xl bg-gray-300 p-4 md:w-[45%] md:basis-1/2 dark:bg-gray-700">
             <div className="mb-2 h-5 w-40 rounded bg-gray-400 dark:bg-gray-600" />
             <div className="h-4 w-32 rounded bg-gray-400 dark:bg-gray-600" />
           </div>
-          <div className="w-[45%] rounded-xl bg-gray-300 p-4 md:basis-1/2 dark:bg-gray-700">
+          <div className="w-full rounded-xl bg-gray-300 p-4 md:w-[45%] md:basis-1/2 dark:bg-gray-700">
             <div className="mb-2 h-5 w-40 rounded bg-gray-400 dark:bg-gray-600" />
             <div className="h-4 w-32 rounded bg-gray-400 dark:bg-gray-600" />
           </div>
-          <div className="w-[45%] rounded-xl bg-gray-300 p-4 md:basis-1/2 dark:bg-gray-700">
+          <div className="w-full rounded-xl bg-gray-300 p-4 md:w-[45%] md:basis-1/2 dark:bg-gray-700">
             <div className="mb-2 h-5 w-40 rounded bg-gray-400 dark:bg-gray-600" />
             <div className="h-4 w-32 rounded bg-gray-400 dark:bg-gray-600" />
           </div>
